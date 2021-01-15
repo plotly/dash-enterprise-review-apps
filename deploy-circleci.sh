d0f364cd14c2c483f455b1384470aa4f8040b1e0
Tobin Ngo <tobin@plot.ly>
2021-01-15 17:47:27 -0500

    update path

--- deploy-circleci.sh
+++ deploy-circleci.sh
@@ -6,15 +6,15 @@ pwd
 ls -al
 
 echo '-----> Creating ssh key'
-echo "$SSH_PRIVATE_KEY" | tr ',' '\n' > ~/circleci/.ssh/id_rsa
-chmod 600 ~/circleci/.ssh/id_rsa # permissioning
+echo "$SSH_PRIVATE_KEY" | tr ',' '\n' > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa # permissioning
 eval "$(ssh-agent -s)" # setting ssh environment variable in shell
 
 echo '-----> Adding keys to ssh-agent'
-ssh-add ~/circleci/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 
 echo '-----> Creating ssh config'
-echo "$SSH_CONFIG" | tr ',' '\n' > ~/circleci/.ssh/config
+echo "$SSH_CONFIG" | tr ',' '\n' > ~/.ssh/config
 
 echo '-----> Adding git remote'
 git config remote.plotly.url >&- || git remote add plotly dokku@dash-playground.plotly.host:aa-tngo-ci-qa # add remote if remote doesn't exist
