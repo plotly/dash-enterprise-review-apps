beca8b097589434f826fa72ba1f85b7264bebec9
Tobin Ngo <tobin@plot.ly>
2021-01-28 23:38:12 -0500

    update

--- app_manager.py
+++ app_manager.py
@@ -11,7 +11,8 @@ if os.getenv("CIRCLECI") == "true":
     script, target_app_name = argv
 
     branch_name = os.getenv("CIRCLE_BRANCH")
-    dash_app_name = f"review-app-{branch_name}".replace("_", "-")
+    # dash_app_name = f"review-app-{branch_name}".replace("_", "-")
+    dash_app_name = "review-app-" + "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
     dash_enterprise_host = os.getenv("DASH_ENTERPRISE_HOST")
     username = os.getenv("USERNAME")
     username_password = os.getenv("USERNAME_PASSWORD")
