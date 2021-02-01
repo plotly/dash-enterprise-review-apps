e1cc29229e419fbb9c374ee7338f9302ab9d8e2e
Chris Parmer <chris@plot.ly>
2021-02-01 17:25:38 -0500

    add a graph

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import dash
 import dash_html_components as html
+import dash_core_components as dcc
 
 app = dash.Dash(__name__)
 server = app.server
 
-app.layout = html.Div([html.H1("hello world")])
+app.layout = html.Div([
+    html.H1("hello world"),
+    dcc.Graph(figure={'data': [{'x': [1, 2, 3], 'y': [2, 1, 6]}]})
+])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
