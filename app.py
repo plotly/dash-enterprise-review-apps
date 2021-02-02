2c9e0e7ac0b81f4f6ee7023a88d690b86c7bc274
Chris Parmer <chris@plot.ly>
2021-02-01 20:14:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import dash_html_components as html
 app = dash.Dash(__name__)
 server = app.server
 
-app.layout = html.Div([html.H1("hello world")])
+app.layout = html.Div([
+    html.H1("hello world - update 1")
+])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
