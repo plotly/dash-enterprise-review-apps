9886810159fb2ff7f6d662d121375c562e910de3
Tobin Ngo <tobin@plot.ly>
2021-02-10 10:39:07 -0500

    prefix

--- .circleci/config.py
+++ .circleci/config.py
@@ -7,10 +7,10 @@ DEBUG = os.getenv("DEBUG", "false")
 if os.getenv("CIRCLECI") == "true":
     print("CIRCLECI")
 
-    PREFIX = "target-dev"
+    PREFIX = "target"
     # PREFIX is a filter for purging review apps.
 
-    LAST_UPDATE = {"hours": 10}
+    LAST_UPDATE = {"hours": 1}
     # LAST_UPDATE is the allowed amount of time before review apps are 
     # purged from the server.
 
